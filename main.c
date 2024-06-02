ee870289cf8c6e555bfdb093bc00149cd453bbb7
renan823 <renan.trofino@gmail.com>
2024-06-02 10:58:07 -0300

    menu base

--- main.c
+++ main.c
@@ -2,41 +2,43 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define TAM_MAX_NOME 100
+
 /*-----------------------------------------------------------------------------------
 Declaração dos tipos usados no programa.
 Os nomes são sugestivos às suas aplicações aqui usadas.
-O tipo 'reserva' relaciona os tipo 'voo' e 'pessoa' por meio 
-do id e cpf (respectivamente).
+O tipo 'reserva' relaciona os tipos 'voo' e 'pessoa'.
 -----------------------------------------------------------------------------------*/
-struct Aviao {
-    capacidade: int;
-    executiva: double;
-    economica: double;
-}
+typedef struct {
+    int capacidade;
+    double executiva;
+    double economica;
+} Aviao;
 
-struct Voo {
-    id: char[4];
-    data: char[10];
-    localPartida: char[4];
-    localChegada: char[4];
-}
+typedef struct Voo {
+    char id[5];
+    char data[11];
+    char localPartida[5];
+    char localChegada[5];
+} Voo;
 
-struct Pessoa {
-    nome: *char;
-    sobrenome: *char;
-    cpf: char[14];
-}
+typedef struct {
+    char *nome;
+    char *sobrenome;
+    char cpf[15];
+} Pessoa;
 
-struct Reserva {
-    cpfPessoa: char[14];
-    idVoo: char[4];
-    assento: char[4];
-    classe: *char;
-    valor: double;
-}
+typedef struct {
+    Pessoa pessoa;
+    Voo voo;
+    char assento[5];
+    char classe[10];
+    double valor;
+} Reserva;
 
 /*-----------------------------------------------------------------------------------
-Funções de utilidades (principalmente para alocação de memória).
+Funções de utilidades (principalmente para alocação de memória 
+e manipulação de arquivos).
 Descrição específica em cada função.
 -----------------------------------------------------------------------------------*/
 
@@ -59,33 +61,22 @@ void *alocar (int quantidade, int tamanho) {
     return (vetor);
 }
 
-/*-----------------------------------------------------------------------------------
-Funções usadas para acessar e manipular os dados. 
-Descrição específica em cada função.
------------------------------------------------------------------------------------*/
-
 /*
-Adiciona uma pessoa na lista de pessoas.
-A pessoa a ser cadastrada, a lista de pessoas e o tamanho da lista
-de pessoas são passados via parâmetro.
-A lista é redimencionada para a inserção
-Nada é retornado.
+Responsável por abrir um arquivo com determinado
+modo de operação (leitura, escritas, etc).
+O nome do arquivo e a operação são passados por parâmetro.
+Se algo der errado, um erro é disparado.
+Senão, um ponteiro para o arquivo é retornado.
 */
-void adicionarPessoa (Pessoa pessoa, Pessoa *pessoas, int tamanho) {
-    
-}
+FILE *abrirArquivo (char *nome, char *operacao) {
+    FILE *arquivo;
 
-/*
-Procura por uma pessoa na lista de pessoas baseado no cpf.
-O cpf, a lista de pessoas e o tamanho da lista de pessoas são passados via parâmetro.
-A pessoa encontrada é retornada.
-*/
-Pessoa encontrarPessoaPorCpf (char cpf[14], Pessoa *pessoas, int tamanho) {
-    for (int i = 0; i < tamanho; i++) {
-        if (pessoas[i].cpf == cpf) {
-            return (pessoas[i]);
-        }
+    if ((arquivo = fopen(nome, operacao)) == NULL) {
+        printf("Erro: não foi possível abrir o arquivo\n");
+        exit(1);
     }
+
+    return (arquivo);
 }
 
 /*-----------------------------------------------------------------------------------
@@ -93,6 +84,100 @@ Funções usadas para coordenar o programa (menu, opções, saída de dados, etc
 Descrição específica em cada função.
 -----------------------------------------------------------------------------------*/
 
+/*
+Decodifica os caracteres para um número simbólico.
+O número determina o tipo de ação.
+O código (char) é recebido por parâmetro.
+O número correspondente é retornado.
+
+1 - Abertura do voo (AV)
+2 - Realizar reserva (RR)
+3 - Consultar reserva (CR)
+4 - Modificar reserva (MR)
+5 - Cancelar reserva (CA)
+6 - Fechamento do dia (FD)
+7 - Fechamento do voo (FV)
+*/
+int decodificar (char codigo[2]) {
+    if (codigo[1] == 'R') {
+        if (codigo[0] == 'R') {
+            return (2);
+        } else {
+            if (codigo[0] == 'C') {
+                return (3);
+            } else {
+                return (4);
+            }
+        }
+    } 
+
+    if (codigo[1] == 'V') {
+        if (codigo[0] == 'A') {
+            return (1);
+        } else {
+            return (7);
+        }
+    }
+
+    if (codigo[0] == 'F') {
+        return (6);
+    }
+    
+    return (5);
+}
+
+/*
+Exibe uma linha com 50 caracteres
+*/
+void linha () {
+    printf("--------------------------------------------------\n");
+}
+
+/*-----------------------------------------------------------------------------------
+Funções usadas para acessar e manipular os dados. 
+Descrição específica em cada função.
+-----------------------------------------------------------------------------------*/
+
+/*
+
+*/
+void aberturaVoo (Aviao *aviao) {
+    scanf("%d %lf %lf", &aviao->capacidade, &aviao->economica, &aviao->executiva);
+}
+
+/*
+Salva uma nova reserva no arquivo.
+Os dados da reserva são passados por parâmetro.
+O arquivo é aberto e os dados são gravados (separadamente, apenas pra facilitar
+a visualização no código).
+Nada é retornado.
+*/
+void adicionarReserva (Reserva reserva) {
+    FILE *reservas = abrirArquivo("voos.txt", "w");
+
+    fprintf(reservas, "%s %s %s ", reserva.pessoa.nome, reserva.pessoa.sobrenome, reserva.pessoa.cpf);
+    fprintf(reservas, "%s %s ", reserva.voo.data, reserva.voo.id);
+    fprintf(reservas, "%s %s %lf ", reserva.assento, reserva.classe, reserva.valor);
+    fprintf(reservas, "%s %s\n", reserva.voo.localPartida, reserva.voo.localChegada);
+
+    fclose(reservas);
+}
+
+void modificarReserva (char *nome, char *sobrenome, char cpf[14], char assento[3]) {
+    FILE *reservas = abrirArquivo("voos.txt", "w");
+
+    fclose(reservas);
+}
+
+void consultarReserva (char cpf[15]) {
+    FILE *reservas = abrirArquivo("voos.txt", "w");
+
+
+
+    fclose(reservas);
+
+    
+}
 
 /*-----------------------------------------------------------------------------------
 Função principal.
@@ -101,7 +186,33 @@ Coordena o funcionamento das outras funções.
 
 int main (void) {
 
+    char codigo[2];
+    int num;
+    Aviao aviao;
 
+    do {
+        scanf("%s", &codigo);
+        
+        num = decodificar(codigo);
+
+        switch (num) {
+            case 1:
+                aberturaVoo(&aviao);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5: 
+                break;
+            case 6: 
+                break;
+            default:
+                printf("fechar voo\n");
+        }
+    } while (num != 7);
 
     return (0);
 }
